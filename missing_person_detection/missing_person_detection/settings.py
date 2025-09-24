eaabbdf21271d3248fd915a731a092528f4f373d
rosh-pawar1821 <roshanipwar.rsp@gmail.com>
2025-09-24 20:12:48 +0530

    Update settings.py for Render deployment

--- missing_person_detection/missing_person_detection/settings.py
+++ missing_person_detection/missing_person_detection/settings.py
@@ -24,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gx@k&%knc4e@+nf0dds+)@5fsigkfsk2%0z_ut&g07r5$he2wt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+ALLOWED_HOSTS = ["missingpersondetectionapp.onrender.com"]
 
 
 # Application definition
