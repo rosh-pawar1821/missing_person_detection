209dc695f945ba79ac9c23900100715ff20ae057
rosh-pawar1821 <roshanipwar.rsp@gmail.com>
2025-09-24 20:13:22 +0530

    Update wsgi.py for Render deployment

--- missing_person_detection/missing_person_detection/wsgi.py
+++ missing_person_detection/missing_person_detection/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "missingpersondetectionapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "missing_person_detection.settings")
 
 application = get_wsgi_application()
